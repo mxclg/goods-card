44a86a9d8edaca024e5be8ae8ac9ff3f04a329de
Ilia <149499467+mxclg@users.noreply.github.com>
2025-06-06 19:10:59 +0200

    feat: add base layout for product card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 function App() {
   return (
-    <div className="p-4">{/* a place for the goods card component */}</div>
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="bg-[#f8f9fb] w-[365px] h-[187px] border border-[#E1EAF6] rounded-[10px] p-4">
+        {/* there will be content */}
+      </div>
+    </div>
   );
 }
 
