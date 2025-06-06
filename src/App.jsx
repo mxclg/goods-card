fae7521e54c609b56be57ced24864a60092dc51a
Ilia <149499467+mxclg@users.noreply.github.com>
2025-06-06 21:02:50 +0200

    chore: add dark logo and product name with arrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import logoDark from "./assets/logo-dark.svg";
+import ArrowRight from "./assets/arrow-right.svg";
+
 function App() {
   return (
     <div className="min-h-screen bg-white flex items-center justify-center">
-      <div className="bg-[#f8f9fb] w-[365px] h-[187px] border border-[#E1EAF6] rounded-[10px] p-4">
-        {/* there will be content */}
+      <div className="bg-[#f8f9fb] w-[365px] h-[187px] border border-[#E1EAF6] rounded-[10px] relative">
+        <div className="absolute top-[20px] left-[15px] w-[60px] h-[60px] bg-white rounded-[5px] overflow-hidden">
+          <img
+            src={logoDark}
+            alt="Product logo"
+            className="w-full h-full object-contain"
+          />
+        </div>
+        <div className="absolute top-[20px] left-[90px] flex items-center gap-[5px] h-[30px]">
+          <span className="text-zinc-800 text-xl font-semibold leading-loose">
+            2Captcha solver
+          </span>
+          <img src={ArrowRight} alt="Arrow" className="w-5 h-5 mt-[2px]" />
+        </div>
       </div>
     </div>
   );
