d4b0e841ee71ff50ab9a675a0d238e0190f4a90b
Ilia <149499467+mxclg@users.noreply.github.com>
2025-06-09 20:30:35 +0200

    feat: change spacing between product cards to 10px

--- src/ProductsLayout.jsx
+++ src/ProductsLayout.jsx
@@ -6,7 +6,7 @@ import twoCaptchaJavascript from "./assets/2Captcha-js-logo.svg";
 
 function ProductsLayout() {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center gap-4">
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center gap-[10px]">
       <ProductCard
         logo={twoCaptchaLogo}
         title="2Captcha solver"
