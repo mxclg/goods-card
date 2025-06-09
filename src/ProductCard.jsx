bfba7e958615ee85dac095f8e6434ef66ecab1d8
Ilia <149499467+mxclg@users.noreply.github.com>
2025-06-09 16:29:09 +0200

    feat: card rise up with large product name

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -13,36 +13,39 @@ function ProductCard({
   isUniversal = true,
 }) {
   return (
-    <div className="bg-[#f8f9fb] w-[365px] h-[187px] border border-[#E1EAF6] rounded-[10px] relative">
-      <div className="absolute top-[20px] left-[15px] w-[60px] h-[60px] bg-white rounded-[5px] overflow-hidden">
-        <img
-          src={logo}
-          alt="Dark logo"
-          className="w-full h-full object-contain"
-        />
-      </div>
-      <div className="absolute top-[20px] left-[90px] flex items-center gap-[5px] h-[30px]">
-        <span className="text-zinc-800 text-xl font-semibold leading-loose">
-          {title}
-        </span>
-        <img src={ArrowRight} alt="Arrow" className="w-5 h-5 mt-[2px]" />
-      </div>
-
-      {isOfficial && (
-        <div className="absolute top-[63px] left-[90px] flex items-center gap-[8px] px-[12px] h-6 bg-[#00A1B3] rounded-2xl">
+    <div className="bg-[#f8f9fb] w-[365px] border border-[#E1EAF6] rounded-[10px] p-4 flex flex-col gap-3">
+      <div className="flex items-start gap-3">
+        <div className="w-[60px] h-[60px] bg-white rounded-[5px] overflow-hidden flex items-center justify-center">
           <img
-            src={logoBrightLock}
-            alt="Bright logo"
-            className="w-3 h-4 object-contain"
+            src={logo}
+            alt="Dark logo"
+            className="w-full h-full object-contain"
           />
-          <span className="text-white text-sm font-normal">
-            Official software
-          </span>
         </div>
-      )}
+        <div className="flex flex-col gap-2 w-64">
+          <div className="flex items-start gap-[5px]">
+            <p className="text-zinc-800 text-xl font-semibold leading-snug break-words">
+              {title}{" "}
+              <img src={ArrowRight} className="inline w-5 h-5" alt="Arrow" />
+            </p>
+          </div>
+          {isOfficial && (
+            <div className="flex items-center gap-[8px] px-[12px] h-6 bg-[#00A1B3] rounded-2xl w-fit">
+              <img
+                src={logoBrightLock}
+                alt="Bright logo"
+                className="w-3 h-4 object-contain"
+              />
+              <span className="text-white text-sm font-normal">
+                Official software
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
 
       {isUniversal && (
-        <div className="absolute top-[107px] left-[15px] flex items-center gap-[8px]">
+        <div className="flex items-center gap-[8px]">
           <img
             src={CircleCheck}
             alt="Check icon"
@@ -54,7 +57,7 @@ function ProductCard({
         </div>
       )}
 
-      <div className="absolute top-[142px] left-[15px] w-[335px] flex items-center justify-between">
+      <div className="flex items-center justify-between">
         <div className="flex items-center gap-[8px]">
           <div className="w-14 h-6 bg-slate-200 rounded-xl flex items-center justify-center gap-[4px] px-[6px]">
             <span className="text-zinc-800 text-sm font-semibold leading-none">
